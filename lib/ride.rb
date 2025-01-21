0c6ca93dee651db58c88fc6f6005fba1a3e41b24
ontruster74 <ontruster74@gmail.com>
2025-01-21 09:31:00 -0700

    add iteration 1-3 class files

---
+++ lib/ride.rb
@@ -0,0 +1,25 @@
+class Ride 
+  attr_reader :name, :min_height, :admission_fee, :excitement, :total_revenue, :rider_log
+
+  def initialize(attributes)
+    @name = attributes[:name]
+    @min_height = attributes[:min_height]
+    @admission_fee = attributes[:admission_fee]
+    @excitement = attributes[:excitement]
+    @total_revenue = 0
+    @rider_log = Hash.new(0)
+  end
+
+  def board_rider(rider)
+    return if !(rider.tall_enough?(@min_height)) || !(rider.preferences.include?(@excitement)) || (rider.spending_money < @admission_fee)
+    @rider_log[rider] += 1
+  end
+
+  def total_rides
+    @rider_log.sum {|_, rides| rides}
+  end
+
+  def total_revenue 
+    self.total_rides * @admission_fee
+  end
+end
---
+++ lib/visitor.rb
@@ -0,0 +1,19 @@
+class Visitor
+  attr_reader :name, :height, :spending_money, :preferences
+
+  def initialize(name, height, spending_money)
+    @name = name
+    @height = height
+    @spending_money = spending_money
+    @preferences = []
+  end
+
+  def add_preference(preference)
+    @preferences << preference
+  end
+
+  def tall_enough?(height_limit)
+    return true if @height >= height_limit
+    return false
+  end
+end
